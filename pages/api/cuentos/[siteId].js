27d28b511ea82fd1b919364bc2388a9816a57ad5
Lucio Villa <lucio310@gmail.com>
2021-04-26 20:36:15 -0400

    remove unused file

--- pages/api/cuentos/[siteId].js
+++
@@ -1,8 +0,0 @@
-import { getAllCuentos } from '../../../lib/db-admin'
-
-export default async (req, res) => {
-  const cuentoId = req.query.siteId
-  const cuentos = await getAllCuentos(cuentoId)
-
-  res.status(200).json({ cuentos })
-}
