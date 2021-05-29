90621d939918590e271e1698a7b5e2a059f71f51
Lucio Villa <lucio310@gmail.com>
2021-05-29 18:14:57 -0400

    make only one CuentosList component

--- components/CuentosList.js
+++ components/CuentosList.js
@@ -1,13 +1,29 @@
 import { parseISO, format } from 'date-fns'
+import DeleteCuentoButton from './DeleteCuentoButton'
+import EditCuentoModal from './EditCuentoModal'
 
-const CuentosList = ({ cuentos }) => {
+const CuentosList = ({ cuentos, simple, admin }) => {
   return (
-    <div className="px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
       {cuentos.map((cuento) => (
         <div className="bg-white p-4 rounded-md shadow-sm" key={cuento.createdAt}>
-          <h3 className="text-md font-bold leading-5">“{cuento.text}”</h3>
-          {cuento.name && <p className="text-sm mt-2">– {cuento.name}</p>}
-          <p className="text-xs text-gray-500 mt-2">{format(parseISO(cuento.createdAt), 'PP')}</p>
+          <h3 className="text-sm text-gray-800 font-medium">“{cuento.text}”</h3>
+          {!simple && (
+            <>
+              {/* {cuento.name && !admin && <p className="text-sm mt-2">– {cuento.name}</p>} */}
+              <p className="text-xs text-gray-500 mt-2">
+                {format(parseISO(cuento.createdAt), 'PP')}
+              </p>
+            </>
+          )}
+          {admin && (
+            <div className="mt-4">
+              <EditCuentoModal cuentoText={cuento.text} cuentoId={cuento.id}>
+                Edit
+              </EditCuentoModal>
+              <DeleteCuentoButton cuentoId={cuento.id} />
+            </div>
+          )}
         </div>
       ))}
     </div>
