39e87ccfb6ef4615f25993443ed863ed73f47ba7
Lucio Villa <lucio310@gmail.com>
2021-05-30 00:25:41 -0400

    increase footer contrast ratio

--- components/Footer.js
+++ components/Footer.js
@@ -4,16 +4,16 @@ const Footer = () => {
   return (
     <div className="pb-8 mt-20 justify-center flex">
       <NextLink href="/">
-        <a className="text-sm mr-4 text-gray-500">Home</a>
+        <a className="text-sm mr-4 text-gray-600">Home</a>
       </NextLink>
       <NextLink href="/about">
-        <a className="text-sm mr-4 text-gray-500">About</a>
+        <a className="text-sm mr-4 text-gray-600">About</a>
       </NextLink>
       <NextLink href="/privacy-policy">
-        <a className="text-sm mr-4 text-gray-500">Privacy Policy</a>
+        <a className="text-sm mr-4 text-gray-600">Privacy Policy</a>
       </NextLink>
       <NextLink href="/terms">
-        <a className="text-sm mr-4 text-gray-500">Terms</a>
+        <a className="text-sm mr-4 text-gray-600">Terms</a>
       </NextLink>
     </div>
   )
