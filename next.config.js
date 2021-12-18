bea22acd7655f91a3ce28c0527807583e90d4152
Lucio Villa <lucio310@gmail.com>
2021-12-18 11:16:13 -0500

    add preact

--- next.config.js
+++ next.config.js
@@ -3,5 +3,16 @@ const withMDX = require('@next/mdx')({
 })
 
 module.exports = withMDX({
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat'
+      })
+    }
+
+    return config
+  },
   pageExtensions: ['js', 'jsx', 'mdx']
 })
